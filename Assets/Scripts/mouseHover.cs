4180e0f661f5cbe2c005bc212291f2e11fe5f2b5
cade1772 <38891423+cade1772@users.noreply.github.com>
2020-02-15 16:43:43 -0600

    added Title screen with input

---
+++ Assets/Scripts/mouseHover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         //= Color.black;
+    }
+
+    void OnMouseEnter()
+    {
+         //= Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        // = Color.black;
+    }
+}
