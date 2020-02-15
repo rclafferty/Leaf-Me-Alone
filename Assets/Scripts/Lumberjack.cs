28b37e9339b2839045aa3d22240ba29b1a87cc30
Robert Lafferty <rclafferty@huskers.unl.edu>
2020-02-14 21:21:58 -0600

    Added lumberjack script

---
+++ Assets/Scripts/Lumberjack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lumberjack : MonoBehaviour
+{
+    bool isStunned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical"); // May or may not use this
+
+        if (isStunned = false)
+        {
+            transform.position = transform.position + (Vector3.right * horizontalInput * 0.1f);
+            transform.position = transform.position + (Vector3.up * verticalInput * 0.1f);
+        }
+    }
+
+    void Hit()
+    {
+        isStunned = true;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,4 +15,9 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    public void Test()
+    {
+
+    }
 }
