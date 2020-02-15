984813b73099ef312df3e5335f1250cf160aea81
cade1772 <38891423+cade1772@users.noreply.github.com>
2020-02-14 21:47:02 -0600

    added swing to lumberjack

--- Assets/Scripts/Lumberjack.cs
+++ Assets/Scripts/Lumberjack.cs
@@ -17,12 +17,22 @@ public class Lumberjack : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical"); // May or may not use this
+        float fire1 = Input.GetAxisRaw("Fire1");
 
-        if (isStunned = false)
+        bool isSwingingAxe = Input.GetButtonDown("Fire1");
+
+        if (isSwingingAxe)
+        {
+            Debug.Log("swing axe");
+        }
+
+        if (isStunned == false)
         {
             transform.position = transform.position + (Vector3.right * horizontalInput * 0.1f);
             transform.position = transform.position + (Vector3.up * verticalInput * 0.1f);
         }
+
+
     }
 
     void Hit()
