f213873b9f53904e11c01ffb5502a17b1d62401e
Robert Lafferty <rclafferty@huskers.unl.edu>
2020-02-15 14:12:38 -0600

    Added glowing stars

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    Animator thisAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAnimator = GetComponent<Animator>();
+        StartCoroutine(Glow(3, 25));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Glow(float minTime, float maxTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+
+            // Animate glow
+            thisAnimator.SetTrigger("Glow");
+
+            yield return new WaitForSeconds(Random.Range(3, 6));
+
+            // Animate dim
+            thisAnimator.SetTrigger("Dim");
+        }
+    }
+}
