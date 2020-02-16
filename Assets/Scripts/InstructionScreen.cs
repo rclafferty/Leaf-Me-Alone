f4a7843dbf26d054b343c75fe2053d7ef9a025e6
Robert Lafferty <rclafferty@huskers.unl.edu>
2020-02-16 11:14:07 -0600

    Added instruction scene -- leads into Main.unity

---
+++ Assets/Scripts/InstructionScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructionScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Continue();
+        }
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
